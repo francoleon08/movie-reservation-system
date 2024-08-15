5a54686510fa7fb04c0911b0852dcbfe2c9ee4ee
francoleon08 <francoleon284@gmail.com>
2024-08-14 22:06:30 -0300

    fix: fix tables genre and language

--- src/main/java/com/moviereservation/movie_reservation_system/models/movies/Genre.java
+++ src/main/java/com/moviereservation/movie_reservation_system/models/movies/Genre.java
@@ -19,7 +19,7 @@ public class Genre {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String name;
 
     @ManyToMany(mappedBy = "genres")
